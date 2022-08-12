2a0ce8c217dec4e00621cad21b5a6dee1698494f
Arun <arungawande1995@gmail.com>
2022-08-13 00:16:52 +0530

    Added: S2 Uc3 ablility for service layer to store the addressbook data

--- src/main/java/com/bridgelabz/addressbook/Dto/AddressBookDto.java
+++ src/main/java/com/bridgelabz/addressbook/Dto/AddressBookDto.java
@@ -15,8 +15,18 @@ public class AddressBookDto {
     private long phoneNumber;
     private String eMail;
 
+//	public long getContactId() {
+//		return contactId;
+//	}
+//
+//	public void setContactId(long contactId) {
+//		this.contactId = contactId;
+//	}
 
-    //	@Override
+    // public AddressBookDto(String string, List<AddressBookModel> dataList) {
+//
+//	}
+    // @Override
 //	public String toString() {
 //		return "AddressBookDto [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
 //				+ city + ", state=" + state + ", zip=" + zip + ", phoneNumber=" + phoneNumber + ", eMail=" + eMail
@@ -25,48 +35,63 @@ public class AddressBookDto {
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getCity() {
         return city;
     }
+
     public void setCity(String city) {
         this.city = city;
     }
+
     public String getState() {
         return state;
     }
+
     public void setState(String state) {
         this.state = state;
     }
+
     public long getZip() {
         return zip;
     }
+
     public void setZip(long zip) {
         this.zip = zip;
     }
+
     public long getPhoneNumber() {
         return phoneNumber;
     }
+
     public void setPhoneNumber(long phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
     public String geteMail() {
         return eMail;
     }
+
     public void seteMail(String eMail) {
         this.eMail = eMail;
     }
